a0177603861b6f905bbbfff481a501ed6f925ba2
durant42040 <b11901040@ntu.edu.tw>
2025-07-31 16:10:25 -0700

    complete proofs from proof search

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -2,46 +2,29 @@ theorem my_add_comm (n m : Nat) : n + m = m + n := by
   rw [Nat.add_comm]
 
 theorem succ_add_succ (a b : Nat) : Nat.succ a + Nat.succ b = Nat.succ (Nat.succ (a + b)) := by
-  sorry
+  simp
+  intros
+  simp [Nat.add_left_comm]
+  ac_rfl
 
 theorem or_swap {p q : Prop} : Or p q → Or q p := by
   sorry
-  /-
-  intro h
-  cases h with
-  | inl hp => exact Or.inr hp   -- came from the left, send it to the right
-  | inr hq => exact Or.inl hq
-  -/
 
-theorem mul_assoc_my (a b c : Nat) : (a * b) * c = a * (b * c) := by
-  sorry
-  /- rw [Nat.mul_assoc] -/
+theorem mul_assoc_my : ∀ a b c : Nat, (a * b) * c = a * (b * c) := by
+  intro a b c
+  simp [Nat.mul_comm]
+  simp [Nat.mul_comm, Nat.mul_left_comm]
 
-theorem my_and_comm {p q : Prop} : And p q → And q p := by
-  sorry
-  /-
-  intro h
-  exact ⟨h.right, h.left⟩
-  -/
+theorem my_and_comm : ∀ {p q : Prop}, And p q → And q p := by
+  simp
+  intro p q hp hq
+  exact ⟨hq, hp⟩
 
 theorem my_not_not_intro {p : Prop} : p → ¬¬p := by
-  sorry
-  /-
-  intro h
-  intro hn
-  exact hn h
-  -/
+  simp
 
 theorem my_imp_self {p : Prop} : p → p := by
-  sorry
-  /-
-  intro h
-  exact h
-  -/
+  simp (config := { contextual := Bool.true })
 
 theorem my_and_true {p : Prop} : p → And p True := by
-  sorry
-  /-
-  intro h
-  exact ⟨h, trivial⟩
-  -/
+  simp
