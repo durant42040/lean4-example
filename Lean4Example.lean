7d55fefc8822c79c4fc800b3141ed24c5e9644c8
durant42040 <b11901040@ntu.edu.tw>
2025-07-31 14:50:56 -0700

    add sorry theorems

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -1,4 +1,47 @@
-theorem succ_add_succ (a b : Nat) : Nat.succ a + Nat.succ b = Nat.succ (Nat.succ (a + b)) := by
-  rw [Nat.succ_add]
-  rfl
+theorem my_add_comm (n m : Nat) : n + m = m + n := by
+  rw [Nat.add_comm]
 
+theorem succ_add_succ (a b : Nat) : Nat.succ a + Nat.succ b = Nat.succ (Nat.succ (a + b)) := by
+  sorry
+
+theorem or_swap {p q : Prop} : Or p q → Or q p := by
+  sorry
+  /-
+  intro h
+  cases h with
+  | inl hp => exact Or.inr hp   -- came from the left, send it to the right
+  | inr hq => exact Or.inl hq
+  -/
+
+theorem mul_assoc_my (a b c : Nat) : (a * b) * c = a * (b * c) := by
+  sorry
+  /- rw [Nat.mul_assoc] -/
+
+theorem my_and_comm {p q : Prop} : And p q → And q p := by
+  sorry
+  /-
+  intro h
+  exact ⟨h.right, h.left⟩
+  -/
+
+theorem my_not_not_intro {p : Prop} : p → ¬¬p := by
+  sorry
+  /-
+  intro h
+  intro hn
+  exact hn h
+  -/
+
+theorem my_imp_self {p : Prop} : p → p := by
+  sorry
+  /-
+  intro h
+  exact h
+  -/
+
+theorem my_and_true {p : Prop} : p → And p True := by
+  sorry
+  /-
+  intro h
+  exact ⟨h, trivial⟩
+  -/
